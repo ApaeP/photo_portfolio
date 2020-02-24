28036a9d3b8be0d00317a2dbfde53ece6c07c357
Paul Portier <paulportier@gmail.com>
2020-02-24 12:50:54 +0100

    photo controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,42 @@
 class PhotosController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @photo.destroy
+    render :index
+  end
+
+  private
+
+  def set_antique
+    @photo = Photo.find(params[:id])
+  end
+
+  def antique_params
+    params.require(:photo).permit(:name, :description, :photo, :category)
+  end
 end
