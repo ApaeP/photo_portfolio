b590bb266aa656c430bb94dbc72cca1931de1c9d
Paul Portier <paulportier@gmail.com>
2021-06-22 04:51:36 +0200

    added link to home from admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: 'Dashboard administrateur'
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do
